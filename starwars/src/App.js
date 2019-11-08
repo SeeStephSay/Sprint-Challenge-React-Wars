72f44269caed1e52c8b2549133d3d6112caa66ca
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-11-08 12:55:15 -0600

    Added reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import StarWarsCard from './components/card';
 import './App.css';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
