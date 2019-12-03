f1618f0ae8761ad3433e700c0e10148b744d03cc
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-12-02 23:47:34 -0600

    Added return statement to CharacterCard

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -4,16 +4,29 @@ import CardData from './CardData';
 
 const CharacterCard = () => {
 	const [ character, setCharacter ] = useState([]);
-  console.log(character);
-  
-  useEffect( () => {
-    axios
-      .get('https://swapi.co/api/people/')
-      .then(response => {
-        console.log("Lo hicimos! We did it!", response.data.results);
-        setCharacter(response.data.results)
-      })
-      .catch(error => {
-        console.log("Oh, maaaaan!", error);
-  });
-}, []);
+	console.log(character);
+
+	useEffect(() => {
+		axios
+			.get('https://swapi.co/api/people/')
+			.then((response) => {
+				console.log('Lo hicimos! We did it!', response.data.results);
+				setCharacter(response.data.results);
+			})
+			.catch((error) => {
+				console.log('Oh, maaaaan!', error);
+			});
+	}, []);
+
+	return (
+		<div>
+			{character.map((item) => {
+				return (
+					<CardData key={item} title={item.name} mass={item.mass} height={item.height} eyecolor={item.eye_color} />
+				);
+			})}
+		</div>
+	);
+};
+
+export default CharacterCard;
